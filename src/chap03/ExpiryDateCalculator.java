5492e8bbf4cb4ec974858054511c1bafb7f09eee
박병욱 <114344042+Rockernun@users.noreply.github.com>
2025-11-14 17:21:48 +0900

    feat(ExpiryDateCalculator): 만료일 계산기 클래스 추가
    - ExpiryDateCalculatorTest 테스트 코드를 통과시킬 만한 최소한의 코드 작성

---
+++ src/chap03/ExpiryDateCalculator.java
@@ -0,0 +1,10 @@
+package chap03;
+
+import java.time.LocalDate;
+
+public class ExpiryDateCalculator {
+
+    public LocalDate calculateExpiryDate(LocalDate date, int payment) {
+        return LocalDate.of(2025, 12, 14);
+    }
+}
