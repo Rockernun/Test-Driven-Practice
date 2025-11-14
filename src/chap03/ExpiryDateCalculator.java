521dd77f83368423a6a76bb6eff65400a1cdecda
박병욱 <114344042+Rockernun@users.noreply.github.com>
2025-11-14 18:55:21 +0900

    refactor(ExpiryDateCalculator): NPE 방지를 위해 null이 아닐 경우에만 처리하도록 리팩토링

--- src/chap03/ExpiryDateCalculator.java
+++ src/chap03/ExpiryDateCalculator.java
@@ -5,6 +5,12 @@ import java.time.LocalDate;
 public class ExpiryDateCalculator {
 
     public LocalDate calculateExpiryDate(PayData paydata) {
+        if (paydata.getFirstBillingDate() != null) {
+            if (paydata.getFirstBillingDate().equals(LocalDate.of(2025, 1, 31))) {
+                return LocalDate.of(2025, 3, 31);
+            }
+        }
+
         return paydata.getBillingDate().plusMonths(1);
     }
 }
