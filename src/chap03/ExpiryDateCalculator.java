665ad225a734072dfa8da410812f3c5a5d76ab4c
박병욱 <114344042+Rockernun@users.noreply.github.com>
2025-11-14 17:26:51 +0900

    refactor(ExpiryDateCalculator): 만료일 계산 메서드 일반화
    - 납부일(billingDate)로부터 한달 이후로 계산

--- src/chap03/ExpiryDateCalculator.java
+++ src/chap03/ExpiryDateCalculator.java
@@ -5,6 +5,6 @@ import java.time.LocalDate;
 public class ExpiryDateCalculator {
 
     public LocalDate calculateExpiryDate(LocalDate date, int payment) {
-        return LocalDate.of(2025, 12, 14);
+        return date.plusMonths(1);
     }
 }
