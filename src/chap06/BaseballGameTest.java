6be8455c20c3d2fe5d0afd16fdcbafc0541cf1dd
박병욱 <114344042+Rockernun@users.noreply.github.com>
2025-11-18 14:44:36 +0900

    test(BaseballGameTest): 숫자 야구 게임에서의 스트라이크 테스트 코드 작성
    - 테스트 코드는 "상황", "실행", "결과 확인" 이라는 3가지 요소로 테스트를 구성할 수 있음
    - 어떤 상황이 주어지고(given), 그 상황에서 기능을 실행하고(when), 실행한 결과를 확인함(then)
    - 주어진 숫자의 값과 자리가 모두 일치하는 경우(스트라이크)에 대한 테스트 추가
    - 현재는 테스트 실패

---
+++ src/chap06/BaseballGameTest.java
@@ -0,0 +1,21 @@
+package chap06;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BaseballGameTest {
+
+    @Test
+    void exactMatch() {
+        // 정답이 456인 상황 (given)
+        BaseballGame game = new BaseballGame("456");
+
+        // 실행 (when)
+        Score score = game.guess("456");
+
+        // 결과 확인 (then)
+        assertEquals(3, score.strikes());
+        assertEquals(0, score.balls());
+    }
+}
