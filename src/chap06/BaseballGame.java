a7dd8540b4849fbf66e7f44a9d76e4f5d26c1f0c
박병욱 <114344042+Rockernun@users.noreply.github.com>
2025-11-18 15:08:45 +0900

    refactor(BaseballGame): 숫자 중복 검사 로직 추가
    - 테스트 통과

--- src/chap06/BaseballGame.java
+++ src/chap06/BaseballGame.java
@@ -1,11 +1,15 @@
 package chap06;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class BaseballGame {
 
     private final String numbers;
 
     public BaseballGame(String numbers) {
         this.numbers = numbers;
+        validateDuplicateNumbers(numbers);
     }
 
     public Score guess(String input) {
@@ -14,5 +18,14 @@ public class BaseballGame {
         }
 
         return new Score(0, 0);
+    }
+
+    private void validateDuplicateNumbers(String input) {
+        Set<String> set = new HashSet<>();
+        for (String s : input.split("")) {
+            if (!set.add(s)) {
+                throw new IllegalArgumentException("숫자는 중복될 수 없습니다.");
+            }
+        }
     }
 }
