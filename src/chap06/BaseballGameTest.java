110aabeb527f4e54eedc416ac48cd2f741218abc
박병욱 <114344042+Rockernun@users.noreply.github.com>
2025-11-18 14:53:16 +0900

    test(BaseballGameTest): 아웃인 경우에 대한 테스트 추가
    - 현재는 테스트 실패

--- src/chap06/BaseballGameTest.java
+++ src/chap06/BaseballGameTest.java
@@ -18,4 +18,17 @@ public class BaseballGameTest {
         assertEquals(3, score.strikes());
         assertEquals(0, score.balls());
     }
+
+    @Test
+    void noMatch() {
+        // 정답이 123인 상황 (given)
+        BaseballGame game = new BaseballGame("123");
+
+        // 실행 (when)
+        Score score = game.guess("456");
+
+        // 결과 확인 (then)
+        assertEquals(0, score.strikes());
+        assertEquals(0, score.balls());
+    }
 }
